b1b372f1d4d821155c89ee61d02c1128c5958b02
Fabiano Lima <fabianosl.dev@gmail.com>
2023-03-12 17:10:27 -0300

    revert(pages): about -> home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'projetos', component: ProjectsComponent},
-  {path: 'sobre', component: AboutComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -29,7 +28,6 @@ import { MatTableModule } from '@angular/material/table'
     AppComponent,
     ProjectsComponent,
     HomeComponent,
-    AboutComponent,
     SideNavComponent,
     ContactFormComponent,
   ],

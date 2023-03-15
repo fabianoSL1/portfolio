a87a6bcc4b2669e301515c4c0856a33c4d6b157c
Fabiano Lima <fabianosl.dev@gmail.com>
2023-03-15 03:00:25 -0300

    feat(service): backend service to store and get messages

---
+++ src/app/services/backend.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Message } from '../interfaces/message';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackendService {
+  private http: HttpClient
+
+  constructor(httpClient: HttpClient) {
+    this.http = httpClient;   
+  }
+
+  postMessage(message: Message) {
+    const url = `${environment.backendApi}/message`;
+    return this.http.post(url, message, {
+      headers: {
+        apiKey: environment.backendApiKey
+      }
+    })
+  }
+
+  getMessages() {
+    const url = `${environment.backendApi}/message`;
+    return this.http.get(url, {
+      headers: {
+        apiKey: environment.backendApiKey
+      }
+    })
+  }
+}
